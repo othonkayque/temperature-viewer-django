b570120cba3c0f841d9d6f6593d0e708c8b7a9c6
othonkayque <kayquepaper@gmail.com>
2023-03-18 11:21:59 -0300

    removing inuseful things from the project

--- showTemperature/app_show_temperature/admin.py
+++ showTemperature/app_show_temperature/admin.py
@@ -1,4 +1,2 @@
 from django.contrib import admin
-from .models import cityNameDB
 
-admin.site.register(cityNameDB)
--- showTemperature/app_show_temperature/models.py
+++ showTemperature/app_show_temperature/models.py
@@ -1,9 +1,3 @@
 from django.db import models
 
-class cityNameDB(models.Model):
-    city = models.CharField(max_length=40)
-    def __str__(self):
-        return self.city
-    @staticmethod
-    def delete_all():
-        cityNameDB.objects.all().delete()
+
